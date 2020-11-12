74257593dbadde17772756b60d64cbaafc59c499
hejie <565703319@qq.com>
2020-11-12 18:35:34 +0800

    [hj] finish practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,17 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch('https://v1.jinrishici.com/all.json', { method: 'GET' })
+    .then((data) => {
+      return data.json();
+    })
+    .then((response) => {
+      const { origin, author, content } = response;
+      const output = [];
+      output.push(origin);
+      output.push(author);
+      output.push(content);
+      return output;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
